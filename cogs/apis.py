070bcdc08f182aefcc613cecce087d020329ae1f
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-15 16:52:10 -0700

    Update apis.py

--- cogs/apis.py
+++ cogs/apis.py
@@ -400,7 +400,7 @@ class Apis(commands.Cog):
                 elif label4 == response["correct_answer"]:
                       await interaction.followup.send(":white_check_mark: Correct answer!")  
           embed=cogs.combinebot.makeEmbed(description=titlequestion, color=discord.Color.blurple())
-          await interaction.response.send_message(embed=embed, view=QuestionView())
+          await interaction.followup.send(embed=embed, view=QuestionView())
 
     @group.command(name="cattext", description="Put text on a random image of a cat!")
     async def cattext(self, interaction, 
